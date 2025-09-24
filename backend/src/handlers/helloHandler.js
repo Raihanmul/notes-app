ecf5c52334f40a19cc1c88114b232d293ef677b6
Raihanmul <raihanmul10@gmail.com>
2025-09-24 19:47:43 +0700

    Membuat post dan getById handler

--- backend/src/handlers/helloHandler.js
+++ backend/src/handlers/helloHandler.js
@@ -1,5 +1,6 @@
 export const sayHello = (req, res) => {
   res.status(200).json({
+    status: "succes",
     message: "Hello, Raihan",
   });
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import { testConnection } from "./config/db.js";
 import helloRouter from "./routes/helloRoute.js";
+import noteRouter from "./routes/notesRoute.js";
 
 const app = express();
-const port = 3000;
 
-app.use("/", helloRouter);
+app.use(express.json());
+
+app.use(helloRouter);
+app.use(noteRouter);
+
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
