e37ab18be9a8fb7d202ff5e93ec566ff4272ad15
Raihanmul <raihanmul10@gmail.com>
2025-09-26 10:33:37 +0700

    update delete method

--- backend/src/routes/notesRoute.js
+++ backend/src/routes/notesRoute.js
@@ -1,8 +1,10 @@
 import express from "express";
 import {
   addNoteHandler,
+  deleteNoteByIdHandler,
   getAllNotesHandler,
   getNoteByIdHandler,
+  updateNoteByIdhandler,
 } from "../handlers/notesHandler.js";
 
 const noteRouter = express.Router();
@@ -10,5 +12,7 @@ const noteRouter = express.Router();
 noteRouter.get("/notes", getAllNotesHandler);
 noteRouter.post("/notes", addNoteHandler);
 noteRouter.get("/notes/:id", getNoteByIdHandler);
+noteRouter.put("/notes/:id", updateNoteByIdhandler);
+noteRouter.delete("/notes/:id", deleteNoteByIdHandler);
 
 export default noteRouter;
