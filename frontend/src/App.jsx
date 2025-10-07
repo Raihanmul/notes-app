ac18538121c7668dbec34ec8958048780a6e513a
Raihanmul <raihanmul10@gmail.com>
2025-10-07 11:11:02 +0700

    add baseUrl

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,11 @@ import { useState } from "react";
 function App() {
   const [notes, setNotes] = useState([]);
 
+  const baseUrl = "https://notes-app-eta-steel.vercel.app"
+
   const fetchNotes = async () => {
     try {
-      const res = await fetch("http://localhost:3000/notes");
+      const res = await fetch(`${baseUrl}/notes`);
 
       const result = await res.json();
 
@@ -22,7 +24,7 @@ function App() {
 
   const addNote = async (newTitle, newContent) => {
     try {
-      const res = await fetch("http://localhost:3000/notes", {
+      const res = await fetch(`${baseUrl}/notes`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ title: newTitle, content: newContent }),
@@ -41,7 +43,7 @@ function App() {
 
   const handleUpdateNote = async (id, updateTitle, updateContent) => {
     try {
-      const res = await fetch(`http://localhost:3000/notes/${id}`, {
+      const res = await fetch(`${baseUrl}/notes/${id}`, {
         method: "PUT",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ title: updateTitle, content: updateContent }),
@@ -60,7 +62,7 @@ function App() {
 
   const handleDelete = async (id) => {
     try {
-      const res = await fetch(`http://localhost:3000/notes/${id}`, {
+      const res = await fetch(`${baseUrl}/notes/${id}`, {
         method: "DELETE",
       });
 
